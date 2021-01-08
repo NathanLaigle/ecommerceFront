8795521e411c00597c5252a1da8dbfd0980be3f2
nathanlaigle <nathanlaigle@gmail.com>
2021-01-08 11:25:17 +0100

    interface : product

---
+++ ecommerceFront/src/app/i/product.ts
@@ -0,0 +1,10 @@
+import { Category } from './category';
+
+export interface Product {
+  productId: number;
+  productName: string;
+  productPrice: number;
+  productImg: string;
+  productDescription: string;
+  productCategory: Category[];
+}
