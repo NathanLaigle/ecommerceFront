43ae69a1d0828f774359f8821037a0f3388a33ad
nathanlaigle <nathanlaigle@gmail.com>
2021-01-08 21:38:48 +0100

    Component : *
    *
    Partials /
    Pages /

--- ecommerceFront/src/app/app.module.ts
+++ ecommerceFront/src/app/app.module.ts
@@ -4,9 +4,17 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoaderComponent } from './c/partials/loader/loader.component';
+import { MobileNavbarComponent } from './c/partials/navbar/mobile-navbar/mobile-navbar.component';
+import { DesktopNavbarComponent } from './c/partials/navbar/desktop-navbar/desktop-navbar.component';
+import { Page404Component } from './c/pages/page404/page404.component';
+import { HomeComponent } from './c/pages/home/home.component';
+import { ProductArchiveComponent } from './c/pages/product-archive/product-archive.component';
+import { ProductSingleComponent } from './c/pages/product-single/product-single.component';
+import { MyAccountComponent } from './c/pages/my-account/my-account.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LoaderComponent, MobileNavbarComponent, DesktopNavbarComponent, Page404Component, HomeComponent, ProductArchiveComponent, ProductSingleComponent, MyAccountComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ ecommerceFront/src/app/c/pages/product-archive/product-archive.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-archive',
+  templateUrl: './product-archive.component.html',
+  styleUrls: ['./product-archive.component.scss']
+})
+export class ProductArchiveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
