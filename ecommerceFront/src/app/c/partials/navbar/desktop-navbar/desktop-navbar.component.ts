9da511dbc7bcb1cbc411f7900698cb53296d4511
nathanlaigle <nathanlaigle@gmail.com>
2021-01-09 16:34:45 +0100

    Component : desktop nav

--- ecommerceFront/src/app/c/partials/navbar/desktop-navbar/desktop-navbar.component.ts
+++ ecommerceFront/src/app/c/partials/navbar/desktop-navbar/desktop-navbar.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/i/category';
+import { CategoryService } from 'src/app/s/category.service';
 
 @Component({
   selector: 'app-desktop-navbar',
   templateUrl: './desktop-navbar.component.html',
-  styleUrls: ['./desktop-navbar.component.scss']
+  styleUrls: ['./desktop-navbar.component.scss'],
 })
 export class DesktopNavbarComponent implements OnInit {
+  constructor(private _categories: CategoryService) {}
 
-  constructor() { }
+  public categories: Category[] = this._categories.category;
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
