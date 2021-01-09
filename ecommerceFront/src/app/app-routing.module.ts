af0a47a3da4a0c72ac0d18bdd55bbaf6d130c349
nathanlaigle <nathanlaigle@gmail.com>
2021-01-09 17:29:38 +0100

    minor changes

--- ecommerceFront/src/app/app-routing.module.ts
+++ ecommerceFront/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './c/pages/home/home.component';
+import { Page404Component } from './c/pages/page404/page404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: '**', component: Page404Component, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
