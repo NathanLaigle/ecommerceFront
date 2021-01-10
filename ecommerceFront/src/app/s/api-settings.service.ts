17400505dfbf7162a53df5ff9d91947d70b38b1b
Marjorie Meziere <mmwebforce20@gmail.com>
2021-01-10 21:06:06 +0100

    Mise à jour partie Angular et réglages pour fonctionnement sur ma machine

--- ecommerceFront/src/app/s/api-settings.service.ts
+++ ecommerceFront/src/app/s/api-settings.service.ts
@@ -13,7 +13,7 @@ export class ApiSettingsService {
 
   // ApiURL
   url = {
-    product: 'https://127.0.0.1:8001/api/post',
+    product: 'https://127.0.0.1:8000/api/post',
     user: 'URL_HERE',
     category: 'URL_HERE',
     order: 'URL_HERE',
